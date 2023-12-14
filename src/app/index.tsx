21e1a8d7dca418acd15ef880b46a7ec6c8b74eb1
Yuri Martins <develop@yumartins.com.br>
2023-12-14 06:57:53 -0300

    feat(app): add loading state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,37 @@
+import { ActivityIndicator, Text, View } from "react-native"
 import { WebView } from "react-native-webview"
-import { styled } from "styled-components/native"
 
-const View = styled(WebView)`
-  flex: 1;
-  margin-top: 64px;
-`
+function Loading() {
+  return (
+    <View
+      style={{
+        gap: 16,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFF",
+      }}
+    >
+      <ActivityIndicator size={24} />
+
+      <Text style={{ color: "#5D5D5D" }}>Loading ...</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View
+    <WebView
+      style={{
+        flex: 1,
+        marginTop: 60,
+      }}
       source={{
         uri: process.env.EXPO_PUBLIC_APP_URL!,
       }}
+      renderLoading={() => <Loading />}
+      geolocationEnabled
+      startInLoadingState
     />
   )
 }
